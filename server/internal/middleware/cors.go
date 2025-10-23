d7c257f18c8ffa4ed6502071f9a2b3f90a827bc8
observable <hello@observable.art>
2025-10-22 22:39:24 -0500

    add cors ip for development server

--- server/internal/middleware/cors.go
+++ server/internal/middleware/cors.go
@@ -9,7 +9,7 @@ import (
 
 func SetupCORS(router *gin.Engine) {
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowOrigins:     []string{"http://localhost:5173", "http://192.168.86.38:5173"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		AllowCredentials: true,
